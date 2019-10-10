c8fee85d854b69be12eca435e6c41c8dcd01e204
Thong Hao Yi <hy.thong@elken.com>
2019-10-10 09:34:26 +0800

    001 - Basic Commit

---
+++ CommandBuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CommandBuild
+//
+//  Created by Thong Hao Yi on 10/10/2019.
+//  Copyright © 2019 Abao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
